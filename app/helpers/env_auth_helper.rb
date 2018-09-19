5cd5e9f531c5c37077234284cb84cd3544749d56
Julian Kalbhenn <jkalbhenn@intera.de>
2018-09-19 12:17:44 +0000

    check for unset variable and if suffix is a string when getting user from environment variable

--- app/helpers/env_auth_helper.rb
+++ app/helpers/env_auth_helper.rb
@@ -2,8 +2,12 @@ module EnvAuthHelper
   def remote_user
     #request.env["HTTP_X_REMOTE_USER"] = ""
     key = request.env[Setting.plugin_redmine_env_auth["env_variable_name"]]
+    return nil unless key
     suffix = Setting.plugin_redmine_env_auth["remove_suffix"]
-    unless suffix.empty? then key = key.chomp suffix end
-    key
+    if suffix.is_a?(String) and not suffix.empty?
+      key.chomp suffix
+    else
+      key
+    end
   end
 end
