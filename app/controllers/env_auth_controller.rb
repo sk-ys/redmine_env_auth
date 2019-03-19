9f27d7dbf667f40c9b5c11d1e146aff8362fd84f
Julian Kalbhenn <jkalbhenn@intera.de>
2019-03-19 10:23:35 +0000

    env_auth/info: display the value before and after applying the remove_suffix option

--- app/controllers/env_auth_controller.rb
+++ app/controllers/env_auth_controller.rb
@@ -1,7 +1,9 @@
 class EnvAuthController < ApplicationController
   helper EnvAuthHelper
   def info
-    name = Setting.plugin_redmine_env_auth["env_variable_name"]
-    render :plain => "variable name: #{name}, value: #{remote_user.inspect}"
+    effective = remote_user
+    variable_name = Setting.plugin_redmine_env_auth["env_variable_name"]
+    original = request.env[variable_name]
+    render :plain => "variable name: #{variable_name}, original value: #{original.inspect}, effective value: #{effective.inspect}"
   end
 end
