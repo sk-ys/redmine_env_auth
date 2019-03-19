ffdf5464f58cd849b8afc4dfeaf0a034fe7ad8bf
Julian Kalbhenn <jkalbhenn@intera.de>
2019-03-19 10:08:10 +0000

    fix env_auth/info returning 404 - render :text has been removed in rails 5. remove skip_method from env_auth_controller because it works without it

--- app/controllers/env_auth_controller.rb
+++ app/controllers/env_auth_controller.rb
@@ -1,13 +1,7 @@
 class EnvAuthController < ApplicationController
-  include EnvAuthHelper
-
-  skip_method = self.respond_to?(:skip_before_filter) ? :skip_before_filter : :skip_before_action
-
-  send(skip_method, :user_setup, :check_if_login_required)
-  helper :env_auth
-
+  helper EnvAuthHelper
   def info
     name = Setting.plugin_redmine_env_auth["env_variable_name"]
-    render :text => "variable name: #{name}<br/>value: #{remote_user.inspect}"
+    render :plain => "variable name: #{name}, value: #{remote_user.inspect}"
   end
 end
