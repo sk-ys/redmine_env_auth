033d0b1beea5719348e4eb039d2012593f910eab
Julian Kalbhenn <jkalbhenn@intera.de>
2019-03-26 10:20:35 +0000

    add listing of available variables on info page

--- app/controllers/env_auth_controller.rb
+++ app/controllers/env_auth_controller.rb
@@ -4,6 +4,15 @@ class EnvAuthController < ApplicationController
     effective = remote_user
     variable_name = Setting.plugin_redmine_env_auth["env_variable_name"]
     original = request.env[variable_name]
-    render :plain => "variable name: #{variable_name}, original value: #{original.inspect}, effective value: #{effective.inspect}"
+    keys = request.env.keys.sort.select {|a|
+      ["action_dispatch.", "action_controller.", "rack.", "puma."].none? {|b| a.start_with?(b)}
+    }.join("\n  ")
+    text = [
+      "variable name: #{variable_name}",
+      "original value: #{original.inspect}",
+      "effective value: #{effective.inspect}",
+      "available variables:\n  #{keys}"
+    ].join("\n")
+    render :plain => text
   end
 end
